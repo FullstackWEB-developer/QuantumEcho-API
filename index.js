c25c90471c557a2c39018bd169bccd02fafa9e99
QianMing <vivanchenko610@gmail.com>
2022-06-11 08:33:20 +0300

    change express

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const { ApolloServer, gql } = require('apollo-server');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+
 const {MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const start = async () => {
+const typeDefs = require('./src/typeDefs');
+const resolvers = require('./src/resolvers');
 
-  const MONGO_DB = process.env.URL
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const httpServer = http.createServer(app);
+
+const startApolloServer = async(app, httpServer) => {
+
+  const MONGO_DB = process.env.MONGO_URL
   const client = await MongoClient.connect(
     MONGO_DB,
     { useNewUrlParser: true, useUnifiedTopology: true}
@@ -17,13 +30,21 @@ const start = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    csrfPrevention: true,
     context,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
 }
-start()
+
+startApolloServer(app, httpServer);
+
+module.exports = httpServer
 
 
